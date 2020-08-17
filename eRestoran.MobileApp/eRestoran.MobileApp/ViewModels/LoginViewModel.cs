a963a03ffe0b6066568a1a270756de2efebf8cfa
Ahmed Terzić <ahmedterzic@hotmail.com>
2020-08-18 01:39:11 +0200

    Creatiing Xamarin Mobile Project, introduction.

---
+++ eRestoran.MobileApp/eRestoran.MobileApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eRestoran.MobileApp.ViewModels
+{
+    public class LoginViewModel:BaseViewModel
+    {
+        string Username = string.Empty;
+        public string _Username
+        {
+            get { return Username; }
+            set { SetProperty(ref Username, value); }
+        }
+        string Password = string.Empty;
+        public string _Password
+        {
+            get { return Password; }
+            set { SetProperty(ref Password, value); }
+        }
+    }
+}
